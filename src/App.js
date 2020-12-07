6ba180d819c161a6971dc99523ec623b7fb943d3
FabioReact <fabio.react.dev@gmail.com>
2020-12-07 17:06:21 +0100

    Fin première journée

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+// import { Fragment } from "react"
 import './App.css';
 
-function App() {
+// Créer un nouveau composant ayant pour nom "Book". Ce composant affichera un élément d'une liste avec le titre d'un livre
+
+const Book = ({ livre = "Nom de livre", auteur = "Nom de l'auteur" }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{livre} - Ecrit par {auteur}</li>
+  )
+}
+
+function App() {
+  const livreFavoris = {
+    titre: "L'étranger",
+    auteur: "Albert Camus",
+  }
+  // Slide aide: https://slides.com/fabiodev/react#/3/4
+
+  return (
+    <>
+      <header>Mon header</header>
+      <ul>
+        <Book livre="Harry Potter" auteur="J. K. Rowlings" ></Book>
+        <Book livre="The Witcher" auteur="Andrzej Sapkowski" />
+        <Book /* Insérer ici les valeurs de livreFavoris */ />
+        <Book />
+      </ul>
+      <footer>Footer</footer>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
+/* const name = "Fabio"
+const element = <h1>Bonjour {name}</h1>
+
+const Button = () => {
+	return <button>Mon beau bouton</button>
+} */
+
+// ReactDOM.render(Button(), document.getElementById("root"))
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
